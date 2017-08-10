40d07bade94a7ecca412a9d9b0a5ee9562d78435
yogita bhor <yogita.bhor@coxandkings.com>
2017-08-10 17:27:43 +0530

    Change in service for file Statistics

--- TLGX_CONSUMER_SERVICE/DataLayer/DL_UploadStaticData.cs
+++ TLGX_CONSUMER_SERVICE/DataLayer/DL_UploadStaticData.cs
@@ -2319,7 +2319,7 @@ namespace DataLayer
                 DataContracts.DC_FileProgressDashboard obj = new DC_FileProgressDashboard();
                 obj.ProgressLog = GetStaticDataUploadProcessLog(new DataContracts.UploadStaticData.DC_SupplierImportFile_Progress_RQ { SupplierImportFile_Id = fileid.ToString() });
                 obj.VerboseLog = GetStaticDataUploadVerboseLog(new DataContracts.UploadStaticData.DC_SupplierImportFile_VerboseLog_RQ { SupplierImportFile_Id = fileid });
-                obj.FileDetails = GetStaticDataFileDetail(new DataContracts.UploadStaticData.DC_SupplierImportFileDetails_RQ { SupplierImportFile_Id = fileid });
+                obj.FileStatistics = GetStaticDataUploadStatistics(new DataContracts.UploadStaticData.DC_SupplierImportFile_Statistics_RQ { SupplierImportFile_Id = fileid});
                 return obj;
             }
             catch (Exception ex)
