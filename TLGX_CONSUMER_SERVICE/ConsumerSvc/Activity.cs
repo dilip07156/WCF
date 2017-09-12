b14f7db61aa67676a8e1b09f33a5594bb2fa2386
anand.desai <anand.desai@WCNKVMNPRJ027>
2017-09-12 10:23:05 +0530

    Activity Search as per new schema

---
+++ TLGX_CONSUMER_SERVICE/ConsumerSvc/Activity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using OperationContracts;
+using DataContracts;
+using BusinessLayer;
+using DataContracts.Masters;
+
+namespace ConsumerSvc
+{
+    public partial class Consumer : IConsumer
+    {
+        #region Activity Search
+        public IList<DC_ActivitySearch_RS> ActivitySearch(DC_Activity_Search_RQ Activity_Request)
+        {
+            using (BL_Activity objBL = new BL_Activity())
+            {
+                List<DC_ActivitySearch_RS> searchResults = new List<DC_ActivitySearch_RS>();
+                searchResults = objBL.ActivitySearch(Activity_Request);
+
+                if (searchResults == null)
+                {
+                    throw new WebFaultException<string>("No records found.", System.Net.HttpStatusCode.NoContent);
+                }
+                return searchResults;
+            }
+        }
+        #endregion
+    }
+}
