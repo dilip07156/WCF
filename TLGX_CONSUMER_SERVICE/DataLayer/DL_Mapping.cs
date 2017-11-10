61bff99fbe52b54ca898fa23751a56dd25499146
Pradeep Gond <pradeep.gond@coxandkings.com>
2017-11-10 12:57:56 +0530

    Minor correction in stateNameWithStateCode

--- TLGX_CONSUMER_SERVICE/DataLayer/DL_Mapping.cs
+++ TLGX_CONSUMER_SERVICE/DataLayer/DL_Mapping.cs
@@ -3915,7 +3915,6 @@ namespace DataLayer
                                                MasterStateCode = ctld.StateCode,
                                                StateCode = a.StateCode,
                                                StateName = a.StateName,
-                                               StateNameWithCode=a.StateName + "("+a.StateCode+")",
                                                Latitude = a.Latitude,
                                                Longitude = a.Longitude
                                            }).Skip(skip).Take(param.PageSize).ToList();
