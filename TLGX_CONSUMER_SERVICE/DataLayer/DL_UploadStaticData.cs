07b66ac5e34c0865272d8b2b0b28ca8648006894
anand.desai <anand.desai@WCNKVMNPRJ027>
2017-12-30 14:38:42 +0530

    mapping changes

--- TLGX_CONSUMER_SERVICE/DataLayer/DL_UploadStaticData.cs
+++ TLGX_CONSUMER_SERVICE/DataLayer/DL_UploadStaticData.cs
@@ -1930,9 +1930,10 @@ namespace DataLayer
                             objNew.UpdateType = obj.UpdateType;
                             objNew.ActionText = obj.ActionText;
                             objNew.StarRating = obj.StarRating;                            
-                            objNew.City_Id =
-                                ((geo.Where(s => s.CityName == obj.CityName).Select(s1 => s1.City_Id).FirstOrDefault())) ??
-                                (geo.Where(s => s.CityCode == obj.CityCode).Select(s1 => s1.City_Id).FirstOrDefault());
+                            objNew.City_Id =                                 
+                                (geo.Where(s => s.CityCode == obj.CityCode).Select(s1 => s1.City_Id).FirstOrDefault())
+                                ??
+                                ((geo.Where(s => s.CityName == obj.CityName).Select(s1 => s1.City_Id).FirstOrDefault()));
                             objNew.Supplier_Id = obj.Supplier_Id;
 
                             if (objNew.CityName == null)
@@ -1948,13 +1949,15 @@ namespace DataLayer
 
                             if (objNew.CountryName == null)
                             {
-                                objNew.CountryName = ((geo.Where(s => s.CityName == objNew.CityName).Select(s1 => s1.CountryName).FirstOrDefault())) ??
-                                (geo.Where(s => s.CityCode == objNew.CityCode).Select(s1 => s1.CountryName).FirstOrDefault());
+                                objNew.CountryName = 
+                                (geo.Where(s => s.CityCode == objNew.CityCode).Select(s1 => s1.CountryName).FirstOrDefault())
+                                ?? ((geo.Where(s => s.CityName == objNew.CityName).Select(s1 => s1.CountryName).FirstOrDefault()));
                             }
                             if (objNew.CountryCode == null)
                             {
-                                objNew.CountryCode = ((geo.Where(s => s.CityName == objNew.CityName).Select(s1 => s1.CountryCode).FirstOrDefault())) ??
-                                (geo.Where(s => s.CityCode == objNew.CityCode).Select(s1 => s1.CountryCode).FirstOrDefault());
+                                objNew.CountryCode = 
+                                (geo.Where(s => s.CityCode == objNew.CityCode).Select(s1 => s1.CountryCode).FirstOrDefault()) 
+                                ?? ((geo.Where(s => s.CityName == objNew.CityName).Select(s1 => s1.CountryCode).FirstOrDefault()));
                             }
                             if (objNew.City_Id != null)
                             {
@@ -1963,8 +1966,9 @@ namespace DataLayer
                             else
                             {
                                 objNew.Country_Id =
-                                ((geo.Where(s => s.CountryName == objNew.CountryName).Select(s1 => s1.Country_Id).FirstOrDefault())) ??
-                                (geo.Where(s => s.CountryCode == objNew.CountryCode).Select(s1 => s1.Country_Id).FirstOrDefault());
+                                    (geo.Where(s => s.CountryCode == objNew.CountryCode).Select(s1 => s1.Country_Id).FirstOrDefault()) ??
+                                ((geo.Where(s => s.CountryName == objNew.CountryName).Select(s1 => s1.Country_Id).FirstOrDefault())) 
+                                ;
                             }
                             context.stg_SupplierProductMapping.Add(objNew);
                         }
