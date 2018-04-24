d2a794a40e70eba0f2de0a44532a99a1842050c1
Ajay Kumar <ajay.kumar@coxandkings.com>
2018-04-24 11:31:33 +0530

    Added new column in stg_RTM and RTM

--- TLGX_CONSUMER_SERVICE/DataLayer/Accommodation_SupplierRoomTypeMapping.cs
+++ TLGX_CONSUMER_SERVICE/DataLayer/Accommodation_SupplierRoomTypeMapping.cs
@@ -64,5 +64,11 @@ namespace DataLayer
         public Nullable<int> MinGuestOccupancy { get; set; }
         public string PromotionalVendorCode { get; set; }
         public string BeddingConfig { get; set; }
+        public string CityName { get; set; }
+        public string CityCode { get; set; }
+        public string StateName { get; set; }
+        public string StateCode { get; set; }
+        public string CountryName { get; set; }
+        public string CountryCode { get; set; }
     }
 }
--- TLGX_CONSUMER_SERVICE/DataLayer/stg_SupplierHotelRoomMapping.cs
+++ TLGX_CONSUMER_SERVICE/DataLayer/stg_SupplierHotelRoomMapping.cs
@@ -51,5 +51,11 @@ namespace DataLayer
         public Nullable<System.Guid> Supplier_Id { get; set; }
         public Nullable<int> MinGuestOccupancy { get; set; }
         public Nullable<System.Guid> SupplierImportFile_Id { get; set; }
+        public string CityName { get; set; }
+        public string CityCode { get; set; }
+        public string StateName { get; set; }
+        public string StateCode { get; set; }
+        public string CountryName { get; set; }
+        public string CountryCode { get; set; }
     }
 }
